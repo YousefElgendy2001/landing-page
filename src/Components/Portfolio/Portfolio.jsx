2be7aceb627bd231ed0eec801d0d730582bc92ef
YousefElgendy2001 <ye9900128@gmail.com>
2025-04-11 16:41:00 +0200

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   src/Components/Portfolio/Portfolio.jsx

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import portHome from "../../assets/images/portHome.png";
-import portCake from "../../assets/images/portCake.png";
-import portCircs from "../../assets/images/portCircs.png";
+
 export default function Portfolio() {
   return (
   <>
